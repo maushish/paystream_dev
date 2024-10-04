0e94f82e485e82c0aff025749575bc315fd4079c
maushish <chamarnumber102@gmail.com>
2024-10-04 14:38:11 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -31,12 +31,12 @@ export default function TypewriterEffectSmoothDemo() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-8">
-        {/* <button className="w-40 h-10 rounded-xl bg-black border border-white text-white text-sm">
-          Join now
-        </button> */}
-        <button className="w-40 h-10 rounded-xl bg-white text-black border text-sm">
+        <button className="w-40 h-10 rounded-xl bg-black border border-white text-white text-sm">
           Pay
         </button>
+        {/* <button className="w-40 h-10 rounded-xl bg-white text-black border text-sm">
+          Pay
+        </button> */}
       </div>
     </div>
   );
