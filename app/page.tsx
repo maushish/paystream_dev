5722a4ee824d72f3bdee77d64b10188c42db46c5
maushish <chamarnumber102@gmail.com>
2024-10-04 14:37:26 +0530

    Updated UX of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ export default function TypewriterEffectSmoothDemo() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-8">
-        <button className="w-40 h-10 rounded-xl bg-black border border-white text-white text-sm">
+        {/* <button className="w-40 h-10 rounded-xl bg-black border border-white text-white text-sm">
           Join now
-        </button>
+        </button> */}
         <button className="w-40 h-10 rounded-xl bg-white text-black border text-sm">
-          Signup
+          Pay
         </button>
       </div>
     </div>
