3114316570f1a274080700afe327b8f74d235637
maushish <chamarnumber102@gmail.com>
2024-10-05 16:27:43 +0530

    CHanged the build

--- app/Forms/page.tsx
+++ app/Forms/page.tsx
@@ -1,26 +1,13 @@
 "use client";
+
 import { HoverEffect } from "../../components/ui/card-hover-effect";
 
-export default function CardHoverEffectDemo() {
-  return (
-
-    <div className="min-h-screen w-full bg-black flex flex-col justify-center items-center">
-<h1 className="text-white text-4xl font-bold">
-  Choose a <span className="text-blue-500">Stream</span>
-</h1>
-      <div className="max-w-5xl px-8">
-        <HoverEffect items={projects}  /> 
-      </div>
-    </div>
-  );
-}
-
-export const projects = [
+const projects = [
   {
     title: "Linear Stream",
     description:
       "Stream suitable for every client and every project. It is a stream that is linear and has a constant flow.",
-    link: "/Linear", // Store link as a string
+    link: "/Linear",
   },
   {
     title: "Cliff Stream",
@@ -29,7 +16,20 @@ export const projects = [
   },
   {
     title: "Step-wise Stream",
-    description:"Stream suitable for contractors that will be paid on weekly/monthly basis for some duration",
+    description: "Stream suitable for contractors that will be paid on weekly/monthly basis for some duration",
     link: "/Stepwise",
   },
 ];
+
+export default function CardHoverEffectDemo() {
+  return (
+    <div className="min-h-screen w-full bg-black flex flex-col justify-center items-center">
+      <h1 className="text-white text-4xl font-bold">
+        Choose a <span className="text-blue-500">Stream</span>
+      </h1>
+      <div className="max-w-5xl px-8">
+        <HoverEffect items={projects} />
+      </div>
+    </div>
+  );
+}
